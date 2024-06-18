e8a893db209e23b9d37e810dc8b74e0426489088
Akib <akibhaider@iut-dhaka.edu>
2024-06-18 23:36:10 +0600

    Main file name changed

--- Simple_Calculator/src/main/java/com/example/lab05_210041222_2b_simple_calculator/Main.java
+++ Simple_Calculator/src/main/java/com/example/lab05_210041222_2b_simple_calculator/Main.java
@@ -3,17 +3,16 @@ package com.example.lab05_210041222_2b_simple_calculator;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("JavaFx Calculator");
+        stage.setTitle("Simple Calculator");
         stage.setScene(scene);
         stage.show();
     }
